b13469814e29c02e2bfa9641037ab739689af466
Kevin Paiz Ramos <paizkevin27@gmail.com>
2025-02-20 22:19:57 -0500

    Added a random component just so i can push the  component folder into github

--- frontend/src/pages/landing-page.jsx
+++ frontend/src/pages/landing-page.jsx
@@ -1,8 +1,10 @@
 import { React } from "react";
+import Header from "../components/header";
 
 const LandingPage = () => {
   return (
     <div>
+      <Header />
       <h1>Welcome to the Landing Page!</h1>
     </div>
   );
