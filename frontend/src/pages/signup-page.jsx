314f8322e1b2889e71159a7bb3a60117d4db02cc
Kevin Paiz Ramos <paizkevin27@gmail.com>
2025-02-21 10:47:05 -0500

    Added a grid to place the elements for the sign up form and the logo

--- frontend/src/pages/signup-page.jsx
+++ frontend/src/pages/signup-page.jsx
@@ -2,8 +2,15 @@ import React from "react";
 
 const SignupPage = () => {
   return (
-    <div>
-      <h1>Welcome to the Signup Page!</h1>
+    <div className="grid-container">
+      {/* left side where the logo will go  */}
+      <div className="left-section">
+        <h1>LogoFJKEGFAWJGEWFWQIEUFGQEUFGEUGEUFQ</h1>
+      </div>
+
+      {/* right side where the form will go */}
+
+      <div className="right-section"></div>
     </div>
   );
 };
