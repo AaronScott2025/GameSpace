73ef609e4e33c6a295dca285d4fccea745432452
Kevin Paiz Ramos <paizkevin27@gmail.com>
2025-02-23 21:28:39 -0500

    Created the sign up form for the front, create the form textfield component for the login, create the submit component to submit the form

--- frontend/src/pages/signup-page.jsx
+++ frontend/src/pages/signup-page.jsx
@@ -1,17 +1,56 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import SignUpTextField from "../components/login-signup-components/sign-up-textfield";
+import FormButton from "../components/login-signup-components/form-button";
 const SignupPage = () => {
+  const [usersData, setUsersData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
   return (
-    <div className="grid-container">
-      {/* left side where the logo will go  */}
-      <div className="left-section">
-        <h1>LogoFJKEGFAWJGEWFWQIEUFGQEUFGEUGEUFQ</h1>
-      </div>
+    <>
+      <form className="form-container">
+        <h1 className="form-tittle">Sign Up</h1>
+        <SignUpTextField
+          label="Username"
+          type="text"
+          name="username"
+          placeholder="username"
+          value={usersData.username}
+          onChange={(e) =>
+            setUsersData({ ...usersData, username: e.target.value })
+          }
+        />
+        <SignUpTextField
+          label="Email"
+          type="email"
+          name="email"
+          placeholder="email"
+          value={usersData.email}
+          onChange={(e) =>
+            setUsersData({ ...usersData, email: e.target.value })
+          }
+        />
+        <SignUpTextField
+          label="Password"
+          type="password"
+          name="password"
+          placeholder="password"
+          value={usersData.password}
+          onChange={(e) =>
+            setUsersData({ ...usersData, password: e.target.value })
+          }
+        />
 
-      {/* right side where the form will go */}
+        <h3>Create account</h3>
 
-      <div className="right-section"></div>
-    </div>
+        <FormButton text="Sign Up" />
+
+        <h3>Already have an account?</h3>
+        <FormButton text="Log In" />
+      </form>
+    </>
   );
 };
 export default SignupPage;
