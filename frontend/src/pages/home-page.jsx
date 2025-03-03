3949e3b70b8898c8d89e47b84b17c26cbee0a9b2
Kevin Paiz Ramos <paizkevin27@gmail.com>
2025-03-03 13:02:23 -0500

    protected routes are done

--- frontend/src/pages/home-page.jsx
+++ frontend/src/pages/home-page.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { supabase } from "../../client";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+  const signOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) throw error;
+    navigate("/login");
+  };
   return (
     <div>
-      <h1>Welcome to the Home Page!</h1>
+      <h1>Hello you are log in</h1>
+      <button onClick={signOut}>sign Out</button>
     </div>
   );
 };
