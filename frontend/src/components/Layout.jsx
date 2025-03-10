e672afcc46d208cb6a48efdbde0faabd1f6209ff
Kevin Paiz Ramos <paizkevin27@gmail.com>
2025-03-10 09:50:38 -0400

    hide the nav bar for the log in, landpage, and sign up pages

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Navbar from "./nav-bar";
+import { useLocation } from "react-router-dom";
 
 const Layout = ({ children }) => {
+  const location = useLocation();
+  const hideNavbarPaths = ["/login", "/signup", "/"];
   return (
     <div>
-      <Navbar />
+      {!hideNavbarPaths.includes(location.pathname) && <Navbar />}
       <div>{children}</div>
     </div>
   );
