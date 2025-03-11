c871d72d201178c4b82dd960e1563f3b099d2322
RBiersach <robertbiersach01@gmail.com>
2025-03-10 21:02:23 -0400

    Moved remaining css files to the Styles folder and updated corresponding jsx files.

--- frontend/src/pages/account-page.jsx
+++ frontend/src/pages/account-page.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '/src/Styles/account-page.css'; 
+import '/src/styles/account-page.css'; 
 
 
 const AccountPage = () => {
--- frontend/src/pages/home-page.jsx
+++ frontend/src/pages/home-page.jsx
@@ -1,5 +1,5 @@
 import Navbar from "../components/nav-bar";
-import "./home-page.css";
+import "/src/styles/home-page.css";
 import React, { useContext, useState, useEffect } from "react";
 import { supabase } from "../../client";
 import { useNavigate } from "react-router-dom";
