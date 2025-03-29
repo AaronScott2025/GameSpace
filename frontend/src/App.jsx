93382c8887c25a99b8a378730e12c9973ee878ca
Praj003 <prajwols03@gmail.com>
2025-03-29 15:13:38 -0400

    dm-page Frontend SetUp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import SignupPage from "./pages/signup-page";
 import HomePage from "./pages/home-page";
 import DuoMatchmakerPage from "./pages/duo-matchmaker-page";
 import ChatBot from "./pages/chat-bot";
+import DMPage from "./pages/dm-page";
 import Wrapper from "./pages/Wrapper";
 import AccountPage from "./pages/account-page";
 import "./App.css";
@@ -22,6 +23,10 @@ function App() {
     {
       path: "/signup",
       element: <SignupPage />,
+    },
+    {
+      path: "/dm-page",
+      element: <DMPage />,
     },
     {
       path: "/chatbot",
