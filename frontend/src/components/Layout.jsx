8c79a9465a05b72ed133c0de4410fab27049064d
Kevin Paiz Ramos <paizkevin27@gmail.com>
2025-04-01 22:21:24 -0400

    use of the spread operator

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -6,7 +6,7 @@ import { useLocation } from "react-router-dom";
 const Layout = ({ children }) => {
   const location = useLocation();
   const hideNavbarPaths = ["/login", "/signup", "/"]; // Add paths where you want to hide the navbar
-  const hideDMButtonPaths = ["/login", "/signup", "/dm-page"]; // Add paths where you want to hide the DM button
+  const hideDMButtonPaths = [...hideNavbarPaths, "/dm-page"]; // Add paths where you want to hide the DM button
   return (
     <div className="layout">
       {!hideNavbarPaths.includes(location.pathname) && <Navbar />}
