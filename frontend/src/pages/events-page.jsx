c46c7b7f5d86b837bc8abc95ae793d2dd88d4058
Kevin Paiz Ramos <paizkevin27@gmail.com>
2025-04-14 13:22:42 -0400

    event infoWindow setup

--- frontend/src/pages/events-page.jsx
+++ frontend/src/pages/events-page.jsx
@@ -1,10 +1,16 @@
-import React, { useState, useEffect, use } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "../styles/events-page.css"; // Import your CSS file for styling
 import { supabase } from "../../client.js"; // Shared client
 import ButtonModal from "../components/button-modal";
 import { IoMdAdd } from "react-icons/io";
 import EventsCard from "../components/event-card.jsx";
-import { APIProvider, Map, AdvancedMarker } from "@vis.gl/react-google-maps";
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  InfoWindow,
+  useAdvancedMarkerRef,
+} from "@vis.gl/react-google-maps";
 import { FaLocationDot } from "react-icons/fa6";
 import {
   useGeolocation,
@@ -23,6 +29,17 @@ function EventsPage() {
   const { data: events, error: tableWithTagsError } = useEventswithTags();
   const { geolocations, error: evntGeoError } = useEventsToGeo();
   const [selectedFilter, setSelectedFilter] = useState("all-events"); // Default filter
+  const [infoWindowShown, setInfoWindowShown] = useState(false);
+  const [markerRef, marker] = useAdvancedMarkerRef();
+
+  // clicking the marker will toggle the infowindow
+  const handleMarkerClick = useCallback(
+    () => setInfoWindowShown((isShown) => !isShown),
+    []
+  );
+
+  // if the maps api closes the infowindow, we have to synchronize our state
+  const handleClose = useCallback(() => setInfoWindowShown(false), []);
 
   const handleFilterChange = (filterName) => {
     setSelectedFilter(filterName); // Update the selected filter
@@ -174,11 +191,23 @@ function EventsPage() {
               {geolocations.map((event) =>
                 event.lat && event.lng ? (
                   <AdvancedMarker
+                    ref={markerRef}
                     key={`${event.event_id}-marker`}
-                    id={`${event.event_id}-marker`}
                     position={{ lat: event.lat, lng: event.lng }}
-                  ></AdvancedMarker>
+                    onClick={handleMarkerClick}
+                  />
                 ) : null
+              )}
+              {infoWindowShown && (
+                <InfoWindow anchor={marker} onClose={handleClose}>
+                  <EventsCard
+                    is_Online={true} //remember this is a boolean
+                    eventName={"Event Name"}
+                    date={"Date"}
+                    location={"Location"}
+                    tags={["tag1", "tag2"]}
+                  />
+                </InfoWindow>
               )}
             </Map>
           </div>
